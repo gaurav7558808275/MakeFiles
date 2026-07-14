fda41e9836fc0ab8f3234b8e6eb978d6a9b56d39
gaurav7558808275 <grvgaurav03@gmail.com>
2026-07-14 16:50:27 +0000

    In process to learn shared object - some error popping, let me see after some sleep

---
+++ project-1/src/Mod-2/header.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "header.h"
+void function_2()
+{
+	std::cout << "Function is called \n";
+}
---
+++ project-1/src/Mod-2/test_mod.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "header.h"
+int main()
+{
+	function_2();
+	return 0;
+}
+
